c79494236a052744f9e4b96614df97ad61195c6c
Muditrastogi08 <75464310+Muditrastogi08@users.noreply.github.com>
2020-12-17 22:30:02 +0530

    Create changechar.c

---
+++ changechar.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    char name []="asus";
+    char company []="sony";
+    int age= 21;
+    printf("my name is Mudit Rastogi\n");
+    printf("I have a %s computer\n",name);
+    printf("I have %s mobile\n",company);
+    printf("my age is %d\n",age);
+    return 0;
+}
