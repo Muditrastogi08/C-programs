62043eb647846e7c7f42e3a016c552e3ca5a62ba
Muditrastogi08 <75464310+Muditrastogi08@users.noreply.github.com>
2020-12-18 15:36:04 +0530

    Create switch.c

---
+++ switch.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a;
+    printf("Enter the Company name\n  ");
+    scanf("%d",&a);
+    
+    switch(a)
+    {
+        case 10:
+        printf("\nMicromax");
+        break;
+        case 20:
+        printf("\nAsus");
+        break;
+    
+    }    
+
+    return 0;
+}
