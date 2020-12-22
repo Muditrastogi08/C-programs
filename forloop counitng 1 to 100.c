ee23d6e300ea0b9437331be1ac7a53c02ca0c5df
Muditrastogi08 <75464310+Muditrastogi08@users.noreply.github.com>
2020-12-23 00:03:45 +0530

    Create forloop counitng 1 to 100.c

---
+++ forloop counitng 1 to 100.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int i=1;
+    printf("Enter the counting 1 to 100\n");
+    for(i=1;i<101;i++)
+    printf("%d\n",i);
+
+
+
+    return 0;
+}
